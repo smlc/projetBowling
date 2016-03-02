3a1fafe2b8c30e45f734d265e64edd3a8eecb593
smlc <siesa.marlon@hotmail.fr>
2016-03-02 15:20:45 +0100

    #12

---
+++ src/main/java/m1/dcll/projetbowling/App.java
@@ -0,0 +1,13 @@
+package m1.dcll.projetbowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
